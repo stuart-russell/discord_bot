ec405bbd114c1ed090f73c26e781d76ce881d75b
srussell91 <stuart.russell@protonmail.com>
2020-06-15 23:48:00 +0100

    removed redundant import

--- insult.py
+++ insult.py
@@ -3,10 +3,8 @@ import asyncio
 import os
 import json
 from discord.ext import commands
-from discord_token import BOT_TOKEN
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
 
 bot = commands.Bot(command_prefix='!')
 

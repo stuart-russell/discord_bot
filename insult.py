326644de29a4df91fcd3fdd28c50e86ed1811579
srussell91 <stuart.russell@protonmail.com>
2020-06-15 23:46:41 +0100

    bot works, cool

--- insult.py
+++ insult.py
@@ -1,29 +1,32 @@
 import aiohttp
 import asyncio
 import os
+import json
 from discord.ext import commands
-from token import BOT_TOKEN
+from discord_token import BOT_TOKEN
+
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 
 bot = commands.Bot(command_prefix='!')
 
 insult_endpoint = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
 
-HEADERS = {"Accept": "application/json", "User-Agent": "Shen"}
+HEADERS = {"Accept": "application/json/text/html", "User-Agent": "Shen"}
 
 async def insult(): 
     async with aiohttp.ClientSession() as session: 
-        async with session.get(url=insult_endpoint, headers=HEADERS) as response_boi:             
-            return await response_boi.json()
+        async with session.get(url=insult_endpoint, headers=HEADERS) as res:             
+            return await res.text()
 
 @bot.command(name='fuckyou')
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def insult_someone(ctx): 
-    json = await insult()
-    await ctx.send(json['fuckyou'])
+    result = await insult()
+    await ctx.send(json.loads(result)['insult'])
 
 @bot.event
 async def on_ready(): 
-    print(f"{bot.user.name} has connected. Bit yer pillow hen, he's comin in dry!")
+    print(f"Bite yer pillow hen, {bot.user.name}'s comin in dry!")
 
-bot.run(BOT_TOKEN)
+bot.run(TOKEN)
